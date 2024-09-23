67080bcb5c0a9753ecf479baf89ddc5fe63ff132
Harsh Singh <alexlin2922@gmail.com>
2024-09-23 13:04:09 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import './App.scss';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
+import Button from './comps/button/Button';
 
 function App() {
   return (
@@ -12,17 +13,20 @@ function App() {
       <BrowserRouter>
 
         <Routes>
-          <Route  path = "/"  element={<Home/>}/>
-          <Route  path = "/about"  element={<About/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-          <Route  path = "/contact"  element={<Contact/>}/>
+          <Route path="/contact" element={<Contact />} />
+
+
+
 
 
         </Routes>
-      
-      
+        
+
       </BrowserRouter>
-     
+
     </div>
   );
 }
