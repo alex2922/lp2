3ce718a22d210f4e1a0f82133bf3c228ff16ecfb
ZaighamCodes <mdzaigham27@gmail.com>
2024-09-23 12:24:19 +0530

    added assets

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-
-import './App.scss';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
-import Footer from './comps/footer/Footer';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Footer from "./comps/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
-         <Footer/>
+        <Footer />
         <Routes>
-          <Route  path = "/"  element={<Home/>}/>
-          <Route  path = "/about"  element={<About/>}/>
-
-          <Route  path = "/contact"  element={<Contact/>}/>
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      
-      
       </BrowserRouter>
-     
     </div>
   );
 }
