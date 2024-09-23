fe6523e590f15ed728b5fa4c95d47dbaff95a4aa
Sunil Shelke <119919644+SharpSunil@users.noreply.github.com>
2024-09-23 17:20:31 +0530

    pagetop

--- src/comps/pagetop/Pagetop.js
+++ src/comps/pagetop/Pagetop.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import pagetopvideo from "../../assets/background.webm";
 import "./Pagetop.scss";
 
 function Pagetop(props) {
   return (
     <div className="parent pagetop">
-      <div className="container pagetop container">
+      <video autoPlay loop muted playsInline>
+        {" "}
+        <source src={pagetopvideo} type="video/mp4" />
+      </video>
+      <div className="container pagetop-container">
         <div className="rotor-box">
           <div className="rotor">
-            <div className="text-box">
-              <h3>{props.text}</h3>
-            </div>
+            {/* <h3>{props.text}</h3> */}
+            <h3>Welcome</h3>
           </div>
         </div>
       </div>
