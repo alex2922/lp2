b9848ee769bfb9f7014676c5985babaa3a78ac8e
nitadiwise <nita@diwise.in>
2024-09-23 13:08:01 +0530

    herosection

--- src/comps/hero/Herosection.js
+++ src/comps/hero/Herosection.js
@@ -1,56 +1,87 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import './herosection.scss';
 
 function Herosection() {
-    return (
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
 
+    return (
         <>
             <div className="herosection-parent parent">
 
                 <Swiper
-                    spaceBetween={30}
+                    spaceBetween={0}
                     centeredSlides={true}
                     autoplay={{
                         delay: 2500,
                         disableOnInteraction: false,
                     }}
-                    pagination={{
-                        clickable: true,
+                    navigation={{
+                        prevEl: prevRef.current,
+                        nextEl: nextRef.current,
                     }}
-                    navigation={true}
+                    onSwiper={(swiper) => {
+                        swiper.params.navigation.prevEl = prevRef.current;
+                        swiper.params.navigation.nextEl = nextRef.current;
+                        swiper.navigation.init();
+                        swiper.navigation.update();
+                    }}
                     modules={[Autoplay, Pagination, Navigation]}
                     className="mySwiper"
                 >
 
-                    {/* ----------------------slide1------------- */}
                     <SwiperSlide>
-                        <div className=" slide slide-img1 bg-img-cover ">
+                        <div className="slide slide-img1 bg-img-cover">
                             <div className="overlay"></div>
-                            <div className="container hero-slide-container">
-                                <h1>Revolutionising the way you receive healthcare with AxonCare
-                                </h1>
-
+                            <div className="slide-text-container container">
+                                <h1 className='slide-heading'>AI and Tech Revolutionising healthcare Delivery</h1>
                                 <button>Know More</button>
                             </div>
                         </div>
+                    </SwiperSlide>
 
+                    <SwiperSlide>
+                        <div className="slide slide-img2 bg-img-cover">
+                            <div className="overlay"></div>
+                            <div className="slide-text-container container">
+                                <h1 className='slide-heading'>Anytime, Anywhere, In Any Language</h1>
+                                <button>Know More</button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className="slide slide-img3 bg-img-cover">
+                            <div className="overlay"></div>
+                            <div className="slide-text-container container">
+                                <h1 className='slide-heading'>Healthcare Is More Accessible Than Ever With Us</h1>
+                                <button>Join AxonCare</button>
+                            </div>
+                        </div>
                     </SwiperSlide>
 
                 </Swiper>
 
-
+                {/* Custom Navigation Buttons */}
+                <div className="custom-nav-buttons">
+                    <div ref={prevRef} className="swiper-button-prev">
+                    <HiOutlineArrowNarrowLeft />
+                    </div>
+                    <div ref={nextRef} className="swiper-button-next">
+                    <HiOutlineArrowNarrowRight />
+                    </div>
+                </div>
 
             </div>
-
-
-
         </>
-    )
+    );
 }
 
-export default Herosection
+export default Herosection;
