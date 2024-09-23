cb9b3b62f286b35b29fe2c199664cf3351815810
ZaighamCodes <mdzaigham27@gmail.com>
2024-09-23 12:19:49 +0530

    synced

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
+import Footer from './comps/footer/Footer';
 
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
-
+         <Footer/>
         <Routes>
           <Route  path = "/"  element={<Home/>}/>
           <Route  path = "/about"  element={<About/>}/>
