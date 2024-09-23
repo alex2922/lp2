25280fe161bebc486d9a3c7dbafdc121cc9da0cd
Sunil Shelke <119919644+SharpSunil@users.noreply.github.com>
2024-09-23 16:22:50 +0530

    last commit

--- src/comps/about/About_sec.js
+++ src/comps/about/About_sec.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./about.scss";
 import Button from "../button/Button";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const About_sec = () => {
+  useEffect(()=>{
+    AOS.init();
+  })
   return (
     <>
       <div className="aboutsec-parent parent">
         <div className="aboutsec-container container">
           <div className="about-left">
-            <div className="mob-img bg-img-cover"></div>
+            <div className="mob-img bg-img-cover" data-aos="fade-down" data-aos-delay="200"></div>
           </div>
           <div className="about-right">
-            <h2 className="heading">
+            <h2 className="heading" data-aos="fade-down" data-aos-delay="200">
               About <span>Axoncare</span>
             </h2>
-            <p className="desc">
+            <p className="desc" data-aos="fade-down" data-aos-delay="400">
               AxonCare, a service brand by Axonic, is a global healthcare
               network that connects doctors and patients worldwide through
               cutting-edge technology solutions. By integrating the healthcare
@@ -25,7 +30,10 @@ const About_sec = () => {
               overcome barriers of language and geography, receiving care in the
               way that suits you best.
             </p>
-            <Button text="Know More" href="/contact" />
+            <div className="buton-a"data-aos="fade-down" data-aos-delay="600">
+            <Button text="Know More" href="/contact"  />
+            </div>
+            
           </div>
         </div>
       </div>
--- src/comps/header/Header.js
+++ src/comps/header/Header.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu3Line } from "react-icons/ri";
 import "./header.scss";
 import { IoIosMail } from "react-icons/io";
 import { ImLocation2 } from "react-icons/im";
 import { IoCloseSharp } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa6";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // import "../"
 const Header = () => {
   const [nav, setNav] = useState(false);
   const [drop, setDrop] = useState(false);
-
+useEffect(()=>{
+    AOS.init();
+})
   return (
     <>
       <div className="header-parent parent">
@@ -21,30 +26,33 @@ const Header = () => {
 
           {/* Mobile Header */}
           {nav && (
-            <div className="mobile-header">
-              <div className="close" onClick={() => setNav(false)}>
+            <div className="mobile-header" data-aos="fade-down" >
+              <div className="close" onClick={() => setNav(false)} data-aos="fade-down" data-aos-delay="1200">
                 <IoCloseSharp />
               </div>
               <div className="menu-links">
-                <Link onClick={() => setNav(false)}>Home</Link>
-                <Link onClick={() => setNav(false)}>About</Link>
+                <Link onClick={() => setNav(false)} data-aos="fade-down" data-aos-delay="200">Home</Link>
+                <Link onClick={() => setNav(false)} data-aos="fade-down" data-aos-delay="400">About</Link>
                 
                   <ul>
-                    <a href="#" onClick={() => setDrop(true)}>
+                    <a href="#" onClick={() => setDrop(!drop)} data-aos="fade-down" data-aos-delay="600">
                       AxonCare NetWork
+                      <FaAngleDown />
                     </a>
 
-                    {drop && (<div className="menudropdown">
-                      <Link onClick={() => setNav(false)}>one</Link>
-                      <Link onClick={() => setNav(false)}>one</Link>
-                      <Link onClick={() => setNav(false)}>one</Link>
-                      <Link onClick={() => setNav(false)}>one</Link>
+                    {drop && (<div className="menudropdown" >
+                      <Link onClick={() => setNav(false)}>Labs</Link>
+                      <Link onClick={() => setNav(false)}>Pharmacies</Link>
+                      <Link onClick={() => setNav(false)}>Corporates</Link>
+                      <Link onClick={() => setNav(false)}>Elderly Care</Link>
+                      <Link onClick={() => setNav(false)}>Societies</Link>
+                      <Link onClick={() => setNav(false)}>Schools</Link>
                     </div>)}
                   </ul>
                 
-                <Link onClick={() => setNav(false)}>Contact Us</Link>
+                <Link onClick={() => setNav(false)} data-aos="fade-down" data-aos-delay="800">Contact Us</Link>
 
-                <div className="mobile-number">
+                <div className="mobile-number" data-aos-delay="1000">
                   <a href="mailto:info@axonichealth.com" className="contact">
                     <div className="icon">
                       <IoIosMail />
