41c64f9bf23a436148b89a8a512d8be34c032922
Ketan Dudka <ketandudka566123@gmail.com>
2024-09-23 14:33:43 +0530

    push

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Videosec from "../../comps/videosec/Videosec";
 import doctorVideo from "../../assets/videos/doctor_video.webm"
 import Partners from "../../comps/partners/Partners";
+import FourCard from "../../comps/fourCard/FourCard";
 function Home() {
   return (
     <div>
@@ -13,7 +14,7 @@ function Home() {
 
         videoSource={doctorVideo}
       />
-
+<FourCard/>
       <Partners/>
     </div>
 
