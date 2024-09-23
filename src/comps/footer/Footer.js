824a4eeb495e7d03594f4a735dae11cdb383d21e
ZaighamCodes <mdzaigham27@gmail.com>
2024-09-23 13:09:04 +0530

    ok

--- src/comps/footer/Footer.js
+++ src/comps/footer/Footer.js
@@ -1,41 +1,59 @@
 import React from "react";
 import "./footer.scss";
 import { Link } from "react-router-dom";
-import footerimg from "../../assets/logo/logo.webp";
+import footerimg from "../../assets/logo/logow.png";
+import { MdMailOutline } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
 const Footer = () => {
   return (
-    <div className="footer-parent parent">
+    <div className="footer-parent parent bg-img-cover">
       <div className="footer-container container">
-        <div className="logo-box footer-box">
-          <Link to="/" className="footer-logo-box">
-            <img src={footerimg} alt="" className="footer-logo" />
-          </Link>
-          <div className="heading">About Us</div>
-          <p>
-            AxonCare, a service brand by Axonic, is a global healthcare network
-            that connects doctors and patients worldwide through cutting-edge
-            technology solutions.
-          </p>
-        </div>
-        <div className="links-box-container footer-box">
-            <div className="heading">Links</div>
+        <div className="footer-top">
+          <div className="logo-box footer-box">
+            <Link to="/" className="footer-logo-box">
+              <img src={footerimg} alt="" className="footer-logo" />
+            </Link>
+            <h2 className="heading">About Us</h2>
+            <p>
+              AxonCare, a service brand by Axonic, is a global healthcare
+              network that connects doctors and patients worldwide through
+              cutting-edge technology solutions.
+            </p>
+          </div>
+          <div className="links-box-container footer-box">
+            <h2 className="heading">Links</h2>
             <div className="links-box">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contactUS">Contact Us</Link>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contactUS">Contact Us</Link>
             </div>
-        </div>
-        <div className="legals-box footer-box">
-            <div className="heading">Legals</div>
+          </div>
+          <div className="legals-box-container footer-box">
+            <h2 className="heading">Legals</h2>
             <div className="legals-box">
-                <Link>Privacy & Policy</Link>
-                <Link>Terms & Conditions</Link>
+              <Link>Privacy & Policy</Link>
+              <Link>Terms & Conditions</Link>
             </div>
-        </div>
-        <div className="address-box footer-box">
+          </div>
+          <div className="address-box-container footer-box">
+            <h2 className="heading">Address</h2>
             <div className="address-box">
-                <span></span>
+              <span>
+                <MdMailOutline />
+              </span>
+              <p>info@axoncare.com</p>
             </div>
+            <div className="address-box">
+              <span>
+              <MdOutlineLocationOn />
+              </span>
+              <p>3911 Concord Pike #8030, Wilmington, Delaware,19803</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          <p>© Copyright AxonCare 2024. All right reserved.</p>
         </div>
       </div>
     </div>
