c1964c23d4d04ec4b673ecebdbb8a35dd8e292d6
candyjo-72 <jyotsna171999@gmail.com>
2024-09-24 11:30:43 +0530

    page

--- src/comps/pagetop/Pagetop.js
+++ src/comps/pagetop/Pagetop.js
@@ -1,19 +1,38 @@
 import React from "react";
 import pagetopvideo from "../../assets/background.webm";
 import "./Pagetop.scss";
+import ReactPlayer from "react-player";
+
 
 function Pagetop(props) {
   return (
     <div className="parent pagetop">
-      <video autoPlay loop muted playsInline>
-        {" "}
-        <source src={pagetopvideo} type="video/mp4" />
-      </video>
+      <ReactPlayer
+                config={{
+                  file: {
+                    forceVideo: true,
+                    attributes: {
+                      poster: props.thumbnail ,
+                      preload: "metadata",
+                    },
+                  },
+                }}
+                autoPlay
+                loop
+                muted
+                playsinline
+                playing={true}
+                url={pagetopvideo}
+                width="100vw"
+              height="auto"
+              
+              />
+      <div className="overlay"></div>
       <div className="container pagetop-container">
         <div className="rotor-box">
           <div className="rotor">
             <h3>{props.text}</h3>
-            
+          
           </div>
         </div>
       </div>
